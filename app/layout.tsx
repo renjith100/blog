b090982ab5ba2dfb102136ff297511c108f84db0
Renjith Abraham <renjith.com@gmail.com>
2025-02-19 20:19:45 +0530

    fix(analytics): fix posthog provider postion again

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,8 +97,8 @@ export default function RootLayout({
       )}
     >
       <body className="antialiased">
+        <ClientTopScrollProgressBar />
         <CSPostHogProvider>
-          <ClientTopScrollProgressBar />
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="max-w-xl mx-4 mt-8 lg:mx-auto">
               <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
