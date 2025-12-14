ca31467536c64c1f1362e148c9c5dc07ba469f3d
Renjith Abraham <renjith.com@gmail.com>
2025-12-14 19:16:35 +0530

    feat: Implement a sky color palette for headings and links with hover effects, and refactor related component styling.

--- app/components/list-blog-posts.tsx
+++ app/components/list-blog-posts.tsx
@@ -19,12 +19,10 @@ export function ListBlogPosts() {
 					<Link key={post.slug} href={`/blog/${post.slug}`}>
 						<div className="layout-row">
 							<div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-								<p className="post-date w-[160px] paragraph-date text-xl tabular-nums text-neutral-600 dark:text-neutral-400">
+								<p className="post-date w-[160px] text-xl tabular-nums">
 									{formatDate(post.metadata.publishedAt, false)} :
 								</p>
-								<p className="paragraph-preview text-xl text-neutral-800 dark:text-neutral-200">
-									{post.metadata.title}
-								</p>
+								<p className="post-title text-xl">{post.metadata.title}</p>
 							</div>
 						</div>
 					</Link>
