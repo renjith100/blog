bd6825c9cb02daa32cf35b0315f169ab0e087dba
Renjith Abraham <renjith.com@gmail.com>
2025-12-30 22:32:25 +0530

    refactor: update SVG arrow accessibility from `aria-label` to `aria-labelledby` with a linked `title` ID.

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -7,9 +7,9 @@ function ArrowIcon() {
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 			role="img"
-			aria-label="Arrow"
+			aria-labelledby="arrow-title"
 		>
-			<title>Arrow</title>
+			<title id="arrow-title">Arrow</title>
 			<path
 				d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
 				fill="currentColor"
